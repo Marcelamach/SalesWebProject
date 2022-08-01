c3387c53343164be52e8434e0e9944784d41849a
Marcela Machado <marcela.machado@zappts.com.br>
2022-08-01 11:28:58 -0300

    Other entities and second migration

--- SalesWebMVC/Models/Department.cs
+++ SalesWebMVC/Models/Department.cs
@@ -9,5 +9,26 @@ namespace SalesWebMVC.Models
     {
         public string Name { get; set; }
         public int Id { get; set; }
+        public ICollection<Seller> Sellers { get; set; } = new List<Seller>();
+
+        public Department()
+        {
+        }
+
+        public Department(string name, int id)
+        {
+            Name = name;
+            Id = id;
+        }
+
+        public void AddSeller(Seller seller)
+        {
+            Sellers.Add(seller);
+        }
+
+        public double TotalSales(DateTime inicial, DateTime final)
+        {
+            return Sellers.Sum(seller => seller.TotalSales(inicial, final));
+        }
     }
 }
